9e5743f51443c1ba23179f828905b27997b2114c
Brian <brian.kieken@gmail.com>
2024-04-20 20:29:34 +0200

    Page 404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,7 @@
 export function ErrorPage () {
-    return <div>
+    return <div className="error404">
         <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <a href="/">Retourner sur la page d’accueil</a>
     </div>
 }
